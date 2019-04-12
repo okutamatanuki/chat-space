57200bb8c2eafb8a26ed7bb5aae149c78b78d1f6
okutamatanuki <seinoazumi@gmail.com>
2019-04-12 20:40:25 +0900

    Made DB,  Migration File, routes

--- db/migrate/20190412112259_create_messages.rb
+++ db/migrate/20190412112259_create_messages.rb
@@ -1,7 +1,8 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-
+      t.string :text
+      t.string :image
       t.timestamps
     end
   end
