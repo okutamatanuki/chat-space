f2df2e0e4f1a1c35c8925333cc7f08493bca1dc8
okutamatanuki <seinoazumi@gmail.com>
2019-04-22 20:20:25 +0900

    Initial Commit
    間違ってローカルのデータが古いものになってしまったので、送ります。

---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,50 @@
+$(function(){
+  function buildHTML(message){
+    Message = message.text ? message.text : ('');
+    Image = message.image.url? message.image.url : ('');
+
+    var messageContainer = `<div class="message">
+                          <div class="upper-info">
+                            <div class= "upper-info__user">
+                              <p>${ message.user_name }</p>
+                            </div>
+                            <div class="upper-info__date">
+                              <p>${message.date}</p>
+                            </div>
+                          </div>
+                          <div class="lower-message">
+                            <div class="lower-message__text">
+                                <p>${Message}</p>
+                                <img src="${Image}", class="lower-message__image">
+                            </div>
+                          </div>
+                        </div>`;
+    return messageContainer;
+
+  }
+  $("#info").on('submit', function(e){
+    e.preventDefault();
+    var userInput = new FormData(this);
+    var href = window.location.href
+    $.ajax({
+      url: href,
+      type: "POST",
+      data: userInput,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-main').append(html)
+      $('#info')[0].reset();
+
+      $('.input-box__submit-btn').on('click', function() {
+        $('.chat-main').animate({scrollTop: $('.chat-main')[0].scrollHeight}, 'fast');
+        });
+    })
+    .fail(function(){
+      alert('メッセージを入力してください。');
+    })
+  });
+});
